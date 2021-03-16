95167d9fe9e59400819fe01bb6413adcc13fead5
Jake Billo <jake@jakebillo.com>
2021-03-16 15:42:49 -0400

    Add Jake bootstrapping script and vim

--- jbillo-bootstrap.sh
+++ jbillo-bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Jake's home environment. You may want to use this as an example for your own deployment.
+# Ensure /root/cifscreds is populated with the necessary credentials files.
 
 sudo python3 /opt/mediavessel/create-automount.py //192.168.1.50/Data /mnt/5n2 --options "rw,vers=3.02,guest,_netdev,file_mode=0777,dir_mode=0777"
 sudo python3 /opt/mediavessel/create-automount.py //192.168.1.4/Data /mnt/drobo5n --options "rw,vers=3.02,guest,_netdev,file_mode=0777,dir_mode=0777"
