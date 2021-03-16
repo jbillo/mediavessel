fca41552b269cc293291f021ee6818702cd74bff
Jake Billo <jake@jakebillo.com>
2021-03-16 14:47:26 -0400

    Create jbillo-bootstrap.sh

---
+++ jbillo-bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Jake's home environment. You may want to use this as an example for your own deployment.
+
+sudo python3 /opt/mediavessel/create-automount.py //192.168.1.50/Data /mnt/5n2 --options "rw,vers=3.02,guest,_netdev,file_mode=0777,dir_mode=0777"
+sudo python3 /opt/mediavessel/create-automount.py //192.168.1.4/Data /mnt/drobo5n --options "rw,vers=3.02,guest,_netdev,file_mode=0777,dir_mode=0777"
+sudo python3 /opt/mediavessel/create-automount.py //192.168.1.3/Seagate8TB /mnt/seagate8tb --options "rw,vers=3.02,forceuid,file_mode=0777,dir_mode=0777,credentials=/root/cifscreds/windows"
+sudo python3 /opt/mediavessel/create-automount.py //192.168.1.9/Data /mnt/ds418 --options "rw,vers=3.02,file_mode=0777,dir_mode=0777,credentials=/root/cifscreds/nas"
