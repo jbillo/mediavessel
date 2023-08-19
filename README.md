d1d184c0a4ec67eb2109ad47a847b6838ed7ab47
Jake Billo <jake@jakebillo.com>
2023-08-19 18:12:30 +0000

    Update script with recommended Docker install

--- README.md
+++ README.md
@@ -6,11 +6,10 @@ Install Ubuntu 22.04 LTS, with HWE kernel option. Notable installation config in
 * Use GPT/btrfs for root volume 
 * Enable SSH for easy system access, but make sure the host is not directly exposed to the Internet
 
-Then pipe curl to sudo bash, as is the tradition with these web tools:
+Then pipe `wget` (installed by default, curl comes later) to `sudo bash`, as is the tradition when installing software from the Internet:
 
 ```
-sudo apt-get -y install curl
-curl -sfL https://raw.githubusercontent.com/jbillo/mediavessel/main/yolo.sh | sudo bash
+wget -q -O - https://raw.githubusercontent.com/jbillo/mediavessel/main/yolo.sh | sudo bash
 ```
 
 This will create an `/opt/mediavessel` directory with a git clone of the repository so we can run the next series of scripts. If you want to change this beforehand, export `LOCAL_REPO_DIR` to the desired path.
--- yolo.sh
+++ yolo.sh
@@ -28,22 +28,38 @@ DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 DEBIAN_FRONTEND=noninteractive apt-get -y install unattended-upgrades
 
 # Install useful system tools.
-DEBIAN_FRONTEND=noninteractive apt-get -y install git openssh-server cifs-utils vim net-tools apt-transport-https ca-certificates software-properties-common
+DEBIAN_FRONTEND=noninteractive apt-get -y install \
+   curl \
+   git \
+   openssh-server \
+   cifs-utils \
+   vim \
+   net-tools \
+   apt-transport-https \
+   ca-certificates \
+   software-properties-common \
+   gnupg
 
-# Install Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-DEBIAN_FRONTEND=noninteractive apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y install docker-ce
+# Add Docker repo and key; update repo list
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+ 
+echo \
+ "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+ "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+ tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Install docker-compose from linuxserver.io
-curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+apt-get update
 
+# Install Docker and components
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Disabled temporarily
 # Fix xrdp so that it can read the snakeoil certificate. Not critical to the whole operation though so ignore failure.
 # https://linuxize.com/post/how-to-install-xrdp-on-ubuntu-20-04/
-adduser xrdp ssl-cert || true
-systemctl restart xrdp || true
+# adduser xrdp ssl-cert || true
+# systemctl restart xrdp || true
 
 # Clone this repository locally (if needed) and update it
 git clone "https://github.com/jbillo/mediavessel.git" "${LOCAL_REPO_DIR}" || true
